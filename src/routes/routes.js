59177c1737d52d3a1ab8051a314242ca85447ac8
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2023-12-23 15:39:55 +0530

    Update folder name according to convention

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { SignIn, SignUp } from "../containers/authentication";
+import { SignIn, SignUp } from "../containers/Authentication";
 
 export const router = createBrowserRouter([
     { path: '/', element: <SignIn /> },
