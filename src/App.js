d3fd1158ecec6258b2a058960664bac0f106789f
Soumalya Pakrashi <soumalyapakrashi@gmail.com>
2023-12-23 12:50:21 +0530

    Add SignIn page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import { SignIn } from "./containers/authentication";
+
 function App() {
-  return (
-    <div></div>
-  );
+    return(
+        <SignIn />
+    );
 }
 
 export default App;
